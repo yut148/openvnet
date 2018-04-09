af9a4f1591592c2bddc4faf805f63c520b8cc985
Metallion <akieckens@axsh.net>
2018-04-09 14:59:49 +0900

    Ruby 2.4 was having problems building dependencies on centos 6 so trying ruby 2.3 which is working on Centos 7

--- ci/ci.el6/integration_test/multibox/ind-steps/step-sclo-ruby/install.sh
+++ ci/ci.el6/integration_test/multibox/ind-steps/step-sclo-ruby/install.sh
@@ -2,7 +2,7 @@
 
 (
     $starting_group "Install sclo ruby"
-    run_cmd "rpm -q --quiet rh-ruby24"
+    run_cmd "rpm -q --quiet rh-ruby23"
     $skip_group_if_unessecarry
     install_package "centos-release-scl"
     install_package "yum-utils"
@@ -12,5 +12,5 @@
         $skip_step_if_already_done
         run_cmd "yum-config-manager --enable rhel-server-rhscl-6-rpms"
     ) ; prev_cmd_failed
-    install_package "rh-ruby24"
+    install_package "rh-ruby23"
 ) ; prev_cmd_failed
